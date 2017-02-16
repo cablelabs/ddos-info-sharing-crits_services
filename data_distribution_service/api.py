578c99be3cab87f5c95a8ba9a842065c440b2582
Zane Hintzman <z.hintzman@cablelabs.com>
2017-02-16 15:36:11 -0700

    Refactored data distribution service to better handle new GET parameters such as 'createdSince' and 'sortBy'.

--- data_distribution_service/api.py
+++ data_distribution_service/api.py
@@ -1,24 +1,22 @@
 from mongoengine import Document, FloatField, IntField, StringField
 from tastypie import authorization
 from tastypie.authentication import MultiAuthentication
-import pymongo
 
 from crits.core.api import CRITsApiKeyAuthentication, CRITsSessionAuthentication
 from crits.core.api import CRITsSerializer, CRITsAPIResource
 from crits.core.crits_mongoengine import CritsDocument
-from crits.core.user_tools import get_user_organization, user_sources
+from crits.core.user_tools import get_user_organization
 from crits.ips.ip import IP
 from crits.vocabulary.objects import ObjectTypes
 
-from django.db.models import F
-
+from handlers import create_raw_query, get_limit
 
 
 class DataDistributionObject(CritsDocument, Document):
     """
     Class to store data for GET requests.
     """
-    ip_address = StringField()
+    ip_address = StringField(verbose_name='IPaddress')
     number_of_times = IntField()
     first_seen = StringField()
     last_seen = StringField()
@@ -33,6 +31,39 @@ class DataDistributionObject(CritsDocument, Document):
     longitude = FloatField()
     attack_types = StringField()
 
+    field_name_to_display_mapping = {
+        'ip_address': 'IPaddress',
+        'number_of_times': 'numberOfTimesSeen',
+        'first_seen': 'firstTimeSeen',
+        'last_seen': 'lastTimeSeen',
+        'total_bps': 'totalBPS',
+        'total_pps': 'totalPPS',
+        'peak_bps': 'peakBPS',
+        'peak_pps': 'peakPPS',
+        'city': 'City',
+        'state': 'State',
+        'country': 'Country',
+        'latitude': 'Latitude',
+        'longitude': 'Longitude',
+        'attack_types': 'attackTypes'
+    }
+
+    @classmethod
+    def get_display_from_field_name(cls, field_name):
+        if field_name not in cls.field_name_to_display_mapping:
+            return None
+        return cls.field_name_to_display_mapping[field_name]
+
+    @classmethod
+    def get_field_name_from_display_name(cls, input_display_name):
+        for field_name, display_name in cls.field_name_to_display_mapping.items():
+            if display_name == input_display_name:
+                return field_name
+        return None
+
+    @classmethod
+    def get_all_display_names(cls):
+        return cls.field_name_to_display_mapping.values()
 
 class DataDistributionResource(CRITsAPIResource):
     """
@@ -42,23 +73,7 @@ class DataDistributionResource(CRITsAPIResource):
     """
     def __init__(self):
         super(DataDistributionResource, self).__init__()
-        self.field_name_to_display_name = {
-            'ip_address': 'IPaddress',
-            'number_of_times': 'numberOfTimesSeen',
-            'first_seen': 'firstTimeSeen',
-            'last_seen': 'lastTimeSeen',
-            'total_bps': 'totalBPS',
-            'total_pps': 'totalPPS',
-            'peak_bps': 'peakBPS',
-            'peak_pps': 'peakPPS',
-            'city': 'City',
-            'state': 'State',
-            'country': 'Country',
-            'latitude': 'Latitude',
-            'longitude': 'Longitude',
-            'attack_types': 'attackTypes'
-        }
-        self.default_limit = 20
+        self.request = None
 
     class Meta:
         object_class = DataDistributionObject
@@ -79,105 +94,92 @@ class DataDistributionResource(CRITsAPIResource):
         return data
 
     def dehydrate(self, bundle):
-        for key, value in self.field_name_to_display_name.items():
-            if key in bundle.data:
-                if bundle.data[key]:
-                    bundle.data[value] = bundle.data.pop(key)
-                else:
-                    del bundle.data[key]
+        null_fields = []
+        for field_name in bundle.data:
+            if bundle.data[field_name]:
+                display_name = DataDistributionObject.get_display_from_field_name(field_name)
+                # Rename field to desired format.
+                if display_name:
+                    bundle.data[display_name] = bundle.data.pop(field_name)
+            else:
+                null_fields.append(field_name)
+        # Remove fields that have null values.
+        for field in null_fields:
+            del bundle.data[field]
         return bundle
 
+    # TODO: add support for searching on particular fields like FirstSeen, LastSeen, etc.
     def obj_get_list(self, request=None, **kwargs):
         """
         Returns the list of data returned in the 'objects' field of a GET request, but in our case we rename 'objects'
          to 'dis-data'.
 
+        Allowed parameters in request:
+        "limit", "sortBy", "sortOrder", "createdSince", "modifiedSince"
+
         :param request:
         :param kwargs:
         :return: List of objects
         """
-        if not request:
-            request = kwargs['bundle'].request
-        username = request.GET.get('username', '')
-        limit = self.default_limit
-        try:
-            limit = int(request.GET.get('limit', self.default_limit))
-        except (TypeError, ValueError):
-            pass
-        return self.create_list_from_ip_objects(username, limit)
+        if request:
+            self.request = request
+        else:
+            self.request = kwargs['bundle'].request
 
-    # CURRENTLY UNUSED
-    def create_list_from_mongo_client(self, username):
-        client = pymongo.MongoClient()
-        ips = client.crits.ips
-        all_ip_entries = ips.find()
-        return_list = []
-        for ip_entry in all_ip_entries:
-            # Create new object with data only if at least one of the sources in the IP object is a source of the user.
-            if self.is_ip_in_users_network(ip_entry, username):
-                new_obj = DataDistributionObject()
-                new_obj.ip_address = ip_entry['ip']
-                for o in ip_entry['objects']:
-                    if o['type'] == ObjectTypes.ATTACK_TYPE:
-                        new_obj.attack_types = o['value']
-                    elif o['type'] == ObjectTypes.CITY:
-                        new_obj.city = o['value']
-                    elif o['type'] == ObjectTypes.COUNTRY:
-                        new_obj.country = o['value']
-                    elif o['type'] == ObjectTypes.NUMBER_OF_TIMES_SEEN:
-                        new_obj.number_of_times = o['value']
-                    elif o['type'] == ObjectTypes.STATE:
-                        new_obj.state = o['value']
-                    elif o['type'] == ObjectTypes.TIME_FIRST_SEEN:
-                        new_obj.first_seen = o['value']
-                    elif o['type'] == ObjectTypes.TIME_LAST_SEEN:
-                        new_obj.last_seen = o['value']
-                    elif o['type'] == ObjectTypes.TOTAL_BYTES_PER_SECOND:
-                        new_obj.total_bps = o['value']
-                    elif o['type'] == ObjectTypes.TOTAL_PACKETS_PER_SECOND:
-                        new_obj.total_pps = o['value']
-                return_list.append(new_obj)
-        return return_list
+        raw_query = create_raw_query(self.request)
+        limit = get_limit(self.request)
+        ip_entries = IP.objects(__raw__=raw_query)[:limit]
+        data_distribution_object_list = self.create_data_distribution_object_list(ip_entries)
+        return data_distribution_object_list
 
-    # Returns true iff at least one of the sources in the ip_entry is a source of the user with the input name.
-    def is_ip_in_users_network(self, ip_entry, username):
-        #return True
-        ip_sources = ip_entry['source']
-        user_source_name_list = user_sources(username)
-        for source in ip_sources:
-            if source['name'] in user_source_name_list:
-                return True
-        return False
+    def create_data_distribution_object_list(self, ip_entries):
+        object_list = []
+        for ip_entry in ip_entries:
+            new_object = self.create_data_distribution_object_from_ip_entry(ip_entry)
+            object_list.append(new_object)
+        sorted_object_list = self.sorted_data_distribution_object_list(object_list)
+        return sorted_object_list
 
-    # Portions of code in this function are based on get_object_list() in core/api.py
-    def create_list_from_ip_objects(self, username, limit=0):
-        querydict = {}
-        # Use only entries with at least one source in the list of sources the user has access to.
-        source_list = user_sources(username)
-        querydict['source.name'] = {'$in': source_list}
-        all_ip_entries = IP.objects(__raw__=querydict)[:limit]
-        return_list = []
-        for ip_entry in all_ip_entries:
-            new_obj = DataDistributionObject()
-            new_obj.ip_address = ip_entry.ip
-            for o in ip_entry.obj:
-                if o.object_type == ObjectTypes.ATTACK_TYPE:
-                    new_obj.attack_types = o.value
-                elif o.object_type == ObjectTypes.CITY:
-                    new_obj.city = o.value
-                elif o.object_type == ObjectTypes.COUNTRY:
-                    new_obj.country = o.value
-                elif o.object_type == ObjectTypes.NUMBER_OF_TIMES_SEEN:
-                    new_obj.number_of_times = o.value
-                elif o.object_type == ObjectTypes.STATE:
-                    new_obj.state = o.value
-                elif o.object_type == ObjectTypes.TIME_FIRST_SEEN:
-                    new_obj.first_seen = o.value
-                elif o.object_type == ObjectTypes.TIME_LAST_SEEN:
-                    new_obj.last_seen = o.value
-                elif o.object_type == ObjectTypes.TOTAL_BYTES_PER_SECOND:
-                    new_obj.total_bps = o.value
-                elif o.object_type == ObjectTypes.TOTAL_PACKETS_PER_SECOND:
-                    new_obj.total_pps = o.value
-            return_list.append(new_obj)
-        return return_list
+    def create_data_distribution_object_from_ip_entry(self, ip_entry):
+        """
+        Portions of code in this function are based on get_object_list() in core/api.py
+        """
+        new_object = DataDistributionObject()
+        new_object.ip_address = ip_entry.ip
+        for o in ip_entry.obj:
+            if o.object_type == ObjectTypes.ATTACK_TYPE:
+                new_object.attack_types = o.value
+            elif o.object_type == ObjectTypes.CITY:
+                new_object.city = o.value
+            elif o.object_type == ObjectTypes.COUNTRY:
+                new_object.country = o.value
+            elif o.object_type == ObjectTypes.NUMBER_OF_TIMES_SEEN:
+                new_object.number_of_times = o.value
+            elif o.object_type == ObjectTypes.STATE:
+                new_object.state = o.value
+            elif o.object_type == ObjectTypes.TIME_FIRST_SEEN:
+                new_object.first_seen = o.value
+            elif o.object_type == ObjectTypes.TIME_LAST_SEEN:
+                new_object.last_seen = o.value
+            elif o.object_type == ObjectTypes.TOTAL_BYTES_PER_SECOND:
+                new_object.total_bps = o.value
+            elif o.object_type == ObjectTypes.TOTAL_PACKETS_PER_SECOND:
+                new_object.total_pps = o.value
+        return new_object
+
+    def sorted_data_distribution_object_list(self, object_list):
+        """
+        Sort the input list of objects.
+        :param object_list:
+        :return:
+        """
+        sort_by = self.request.GET.get('sortBy', '')
+        if sort_by:
+            field_name = DataDistributionObject.get_field_name_from_display_name(sort_by)
+            if not field_name:
+                raise ValueError("'sortBy' parameter is not a valid field to sort on.")
+            sort_order = self.request.GET.get('sortOrder', 'desc')
+            is_reverse = (sort_order == 'desc')
+            sorted_object_list = sorted(object_list, key=lambda x: getattr(x, field_name), reverse=is_reverse)
+            return sorted_object_list
+        return object_list
---
+++ data_distribution_service/handlers.py
@@ -0,0 +1,123 @@
+from datetime import datetime
+from pymongo import MongoClient
+
+from crits.core.user_tools import user_sources
+
+
+def create_raw_query(request):
+    """
+    :param request:
+    :return: dict
+    """
+    raw_query = {}
+    # Use only entries with at least one source in the list of sources the user has access to.
+    username = request.GET.get('username', '')
+    source_list = user_sources(username)
+    raw_query['source.name'] = {'$in': source_list}
+    # Filter on entries created since the 'createdSince' time.
+    created_since = request.GET.get('createdSince', '')
+    if created_since:
+        created_since_datetime = None
+        try:
+            created_since_datetime = datetime.strptime(created_since, "%Y-%m-%dT%H:%M:%S.%fZ")
+        except (ValueError):
+            try:
+                created_since_datetime = datetime.strptime(created_since, "%Y-%m-%d")
+            except (ValueError):
+                pass
+        if not created_since_datetime:
+            raise ValueError("'createdSince' time not a properly formatted ISO string.")
+        raw_query['created'] = {'$gte': created_since_datetime}
+
+    # Filter on entries modified since the 'modifiedSince' time.
+    modified_since = request.GET.get('modifiedSince', '')
+    if modified_since:
+        modified_since_datetime = None
+        try:
+            modified_since_datetime = datetime.strptime(modified_since, "%Y-%m-%dT%H:%M:%S.%fZ")
+        except (ValueError):
+            try:
+                modified_since_datetime = datetime.strptime(modified_since, "%Y-%m-%d")
+            except (ValueError):
+                pass
+        if not modified_since_datetime:
+            raise ValueError("'modifiedSince' time not a properly formatted ISO string.")
+        raw_query['modified'] = {'$gte': modified_since_datetime}
+    return raw_query
+
+def get_limit(request):
+    input_limit = request.GET.get('limit', '')
+    if input_limit:
+        try:
+            limit_integer = int(input_limit)
+            return limit_integer
+        except (TypeError, ValueError):
+            raise ValueError("'limit' field set to invalid value. Must be integer.")
+    # return an arbitrary default value
+    return 20
+
+
+
+# SHOULD NOT BE USED
+def sort_ip_entries(self, request, ip_entries):
+    # Mapping of possible inputs for "sortBy" field to the names of the fields that are stored as separate objects
+    # within the "objects" field of an IP.
+    inputs_to_nested_object_fields = {
+        "ASName": "AS Name",
+        "ASNumber": "AS Number",
+        "sourcePort": "Source Port",
+        "destinationPort": "Destination Port",
+        "numberOfTimesSeen": "Number of Times Seen",
+        "firstTimeSeen": "Time First Seen",
+        "lastTimeSeen": "Time Last Seen",
+        "totalBPS": "Total BPS",
+        "totalPPS": "Total PPS",
+        "peakBPS": "Peak BPS",
+        "peakPPS": "Peak PPS",
+        "extra": "Extra",
+        "city": "City",
+        "state": "State",
+        "country": "Country",
+        "latitude": "Latitude",
+        "longitude": "Longitude",
+        "attackTypes": "Attack Types"
+    }
+
+    sort_by = request.GET.get('sortBy', '')
+    sort_order = request.GET.get('sortOrder', 'desc')
+    if sort_by:
+        if sort_by in inputs_to_nested_object_fields:
+            client = MongoClient()
+            db = client.crits
+            nested_field = inputs_to_nested_object_fields[sort_by]
+            pipeline = [
+                {'$unwind': '$objects'},
+                {'$match': {'objects.type': nested_field}},
+                {'$group': {'_id': '$ip', nested_field: {"$first": '$objects.value'}}},
+                {'$sort': {nested_field: 1}}
+            ]
+            # this only contains the "nested_field" and IP addresses for all entries, sorted by the "nested_field".
+            # TODO: I want the original objects sorted by the "nested_field".
+            # TODO: Also, I only want to sort the input ip_entries, whereas this currently sorts ALL entries in the
+            # IP collection.
+            aggregation = db.ips.aggregate(pipeline)
+            if aggregation:
+                result = aggregation['result']
+            new_ip_entries = None
+            for result_entry in result:
+                next_ip_entry = ip_entries.filter(ip=result_entry['_id'])
+                if next_ip_entry:
+                    if not new_ip_entries:
+                        new_ip_entries = next_ip_entry
+                    else:
+                        new_ip_entries = new_ip_entries.merge(next_ip_entry)
+            return new_ip_entries
+            # Can't do it this way below without MongoEngine 0.9 or above. Currently 0.8.8
+            # ip_entries = ip_entries.aggregate(pipeline)
+        else:
+            sort_query_string = sort_by
+            if sort_order == 'desc':
+                sort_query_string = '-' + sort_query_string
+            ip_entries = ip_entries.order_by(sort_query_string)
+
+    return ip_entries
