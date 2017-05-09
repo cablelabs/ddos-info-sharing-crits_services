e548be21a4ec216c28451a264bd194b4c6dfda7a
Zane Hintzman <z.hintzman@cablelabs.com>
2017-05-09 16:58:00 -0600

    Modified ASN lookup API to catch various exceptions.

--- IP_ASN_mapping_service/ASNLookup/ipwhois_asn_lookup.py
+++ IP_ASN_mapping_service/ASNLookup/ipwhois_asn_lookup.py
@@ -1,8 +1,8 @@
 from ipwhois import IPWhois
+from ipwhois.exceptions import IPDefinedError, ASNRegistryError, HTTPLookupError
 
-def get_asn_data_from_ipwhois(ip):
-    object = IPWhois(ip)
-    result = object.lookup_rdap(depth=1)
+def get_asn_data_from_ipwhois(ip_address):
+    result = get_rdap_lookup_result(ip_address)
     output = {
         'as_number': result['asn'],
         'country_code': result['asn_country_code']
@@ -10,11 +10,23 @@ def get_asn_data_from_ipwhois(ip):
     return output
 
 def get_as_number_from_ipwhois(ip_address):
-    object = IPWhois(ip_address)
-    result = object.lookup_rdap(depth=1)
+    result = get_rdap_lookup_result(ip_address)
     return result['asn']
 
 def get_country_code_from_ipwhois(ip_address):
-    object = IPWhois(ip_address)
-    result = object.lookup_rdap(depth=1)
+    result = get_rdap_lookup_result(ip_address)
     return result['country_code']
+
+def get_rdap_lookup_result(ip_address):
+    object = IPWhois(ip_address)
+    try:
+        result = object.lookup_rdap(depth=1)
+    except ValueError:
+        raise
+    except IPDefinedError:
+        raise ValueError("IP address " + ip_address + " is reserved for some special purpose.")
+    except ASNRegistryError:
+        raise ValueError("IP address " + ip_address + " does not have entry in ASN registry.")
+    except HTTPLookupError as e:
+        raise Exception(e.message)
+    return result
