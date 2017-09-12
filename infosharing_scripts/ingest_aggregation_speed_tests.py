ad25dd0f212cf8c0dc438d28a0b5bb16336354df
Zane Hintzman <z.hintzman@cablelabs.com>
2017-09-12 17:04:45 -0600

    GET requests to ingest service now run aggregation on Event collection.

--- infosharing_scripts/ingest_aggregation_speed_tests.py
+++ infosharing_scripts/ingest_aggregation_speed_tests.py
@@ -1,85 +1,20 @@
-import json
-from jsonschema import validate, FormatChecker, ValidationError
-from tastypie import authorization
-from tastypie.authentication import MultiAuthentication
+from datetime import datetime
+import os
 
-from crits.core.api import CRITsApiKeyAuthentication, CRITsSessionAuthentication
-from crits.core.api import CRITsSerializer, CRITsAPIResource
-from crits.core.user_tools import get_user_organization, user_sources
+os.environ['DJANGO_SETTINGS_MODULE'] = 'crits.settings'
+
 from crits.events.event import Event
 from crits.ips.ip import IP
-
-from handlers import save_ingest_data
-from vocabulary import IPOutputFields, EventOutputFields
+from data_ingester_service.vocabulary import IPOutputFields, EventOutputFields
 
 
-class DataIngesterResource(CRITsAPIResource):
-    """
-    Class to handle everything related to the Data Ingester API.
-    """
-
-    class Meta:
-        allowed_methods = ('get', 'post')
-        resource_name = "data_ingester_resource"
-        collection_name = "ingestData"
-        excludes = ["id", "resource_uri", "unsupported_attrs"]
-        limit = 20
-        max_limit = 0
-        authentication = MultiAuthentication(CRITsApiKeyAuthentication(),
-                                             CRITsSessionAuthentication())
-        authorization = authorization.Authorization()
-        serializer = CRITsSerializer()
+class IngestAggregationSpeedTest:
 
     def __init__(self):
-        super(DataIngesterResource, self).__init__()
-        # ASSUMPTION: File named 'Data-Ingester-Payload-Schema.json' exists in directory '/data'.
-        path_to_schema = '/data/Data-Ingester-Payload-Schema.json'
-        schema_file = open(path_to_schema, 'r')
-        self.input_schema = json.load(schema_file)
+        self.aggregation_pipeline = []
+        self.is_events = True
 
-    def obj_create(self, bundle, **kwargs):
-        """
-        Handles POST request to add or update IP objects using format specified for MSOs.
-
-        :param bundle: Bundle containing the information to create the Identifier.
-                       Should contain a list of IP-like objects.
-        :type bundle: Tastypie Bundle object.
-        :param kwargs: (Not used.)
-        :returns: (nothing.)
-        """
-        response = {
-            'message': '',
-            'return_code': 1
-        }
-        try:
-            validate(bundle.data, self.input_schema, format_checker=FormatChecker())
-        except ValidationError as e:
-            response['message'] = "Validation Error: " + e.message
-            self.crits_response(response, status=400)
-            return
-        except Exception as e:
-            response['message'] = "Error during schema validation: " + e.message
-            self.crits_response(response, status=500)
-            return
-        source = bundle.data.get('ProviderName')
-        analyst = bundle.request.user.username
-        sources = user_sources(analyst)
-        if source not in sources:
-            response['message'] = "Error: User not allowed to publish to source '" + str(source) + "'."
-            self.crits_response(response, status=403)
-            return
-        try:
-            ingest_data_entries = bundle.data.get('ingestData', None)
-            save_ingest_data(analyst, source, ingest_data_entries)
-        except Exception as e:
-            response['message'] = 'Error saving IP data: ' + e.message
-            self.crits_response(response, status=500)
-            return
-        response['message'] = 'All data has been saved!'
-        response['return_code'] = 0
-        self.crits_response(response)
-
-    def obj_get_list(self, request=None, **kwargs):
+    def run(self):
         """
         Returns the list of data to be sent in the 'outputData' field of a GET request.
 
@@ -87,36 +22,59 @@ class DataIngesterResource(CRITsAPIResource):
         :param kwargs:
         :return: list of objects
         """
-        # PROBLEM: What if users submit their own IP? We distinguished submitter from owner using releasability, but what if submitter was the owner?
-        if request:
-            self.request = request
+        if self.is_events:
+            self._add_aggregation_stages_start_at_events()
         else:
-            self.request = kwargs['bundle'].request
-        #self.aggregation_pipeline = []
-        self._add_aggregation_stages_start_at_events()
-        for stage in self.aggregation_pipeline:
-            print stage
+            self._add_aggregation_stages()
         collation = {
             'locale': 'en_US_POSIX',
             'numericOrdering': True
         }
-        result = Event.objects.aggregate(*self.aggregation_pipeline, allowDiskUse=True, collation=collation, useCursor=False)
-        objects = list(result)
-        return objects
+        print "Stages:"
+        for stage in self.aggregation_pipeline:
+            print stage
+        for i in range(0, len(self.aggregation_pipeline)):
+            sub_pipeline = self.aggregation_pipeline[:(i+1)]
+            print "Doing query for first " + str(i+1) + " stages..."
+            if self.is_events:
+                start = datetime.now()
+                result = Event.objects.aggregate(*sub_pipeline, allowDiskUse=True, collation=collation, useCursor=False)
+                end = datetime.now()
+            else:
+                start = datetime.now()
+                result = IP.objects.aggregate(*sub_pipeline, allowDiskUse=True, collation=collation, useCursor=False)
+                end = datetime.now()
+            duration = end - start
+            print "Time: " + str(duration)
+            if i >= 7:
+                print "Converting results to list..."
+                start = datetime.now()
+                objects = list(result)
+                end = datetime.now()
+                duration = end - start
+                print "Time to convert: " + str(duration)
 
-    # This version aggregates from IP collection.
     def _add_aggregation_stages(self):
         """
         Add all important stages to the aggregation pipeline.
         :return: (nothing)
         """
-        username = self.request.GET.get('username', '')
-        user_organization = get_user_organization(username)
+        # self._match_ips_on_releasability()
+        # self._project_ip_sub_object_fields()
+        # self._lookup_related_events()
+        # self._project_event_fields_to_top_level()
+        # self._group_documents_by_event_id()
+        # self._project_event_fields_to_nested_level()
+        # self._group_documents_by_ip_with_event_data()
+        # self._project_ip_address()
+        # self._project_event_fields()
+        # self._add_sort_to_pipeline()
+        user_organization = 'Comcast'
         # TODO: This may not be correct way to get user submitted data, so fix it.
+        # Source name should definitely match user org, or else they neither submitted it nor own it.
         match_user_submissions_stage = {
             '$match': {
                 'source.name': user_organization,
-                'releasability.name': {'$ne': user_organization}
             }
         }
         project_cleanup_ip_fields_stage = {
@@ -138,11 +96,11 @@ class DataIngesterResource(CRITsAPIResource):
         }
         # NOTE: Even though we unwind 'event', there should be only one Event because the ingest service creates only
         # one relationship to any given Event.
-        unwind_event_stage = {'$unwind': '$'+EVENT_FIELD}
+        unwind_event_stage = {'$unwind': '$' + EVENT_FIELD}
         # Filter events based on what user submitted, since other users may have submitted events to the same IPs.
         match_event_source_stage = {
             '$match': {
-                EVENT_FIELD+'.source.name': user_organization
+                EVENT_FIELD + '.source.name': user_organization
             }
         }
         # This field is used simply to sort the data based approximately on the time the user submitted these events.
@@ -154,7 +112,7 @@ class DataIngesterResource(CRITsAPIResource):
                 EVENT_TIME_RECORDED_FIELD: {
                     '$dateToString': {
                         'format': '%Y-%m-%dT%H:%M:%S.%LZ',
-                        'date': '$'+EVENT_FIELD+'.created'
+                        'date': '$' + EVENT_FIELD + '.created'
                     }
                 },
                 EventOutputFields.ATTACK_TYPES: {
@@ -163,7 +121,7 @@ class DataIngesterResource(CRITsAPIResource):
                     '$map': {
                         'input': {
                             '$filter': {
-                                'input': '$'+EVENT_FIELD+'.objects',
+                                'input': '$' + EVENT_FIELD + '.objects',
                                 'as': 'obj',
                                 'cond': {'$eq': ['$$obj.type', attack_type_sub_object_type]}
                             }
@@ -184,7 +142,7 @@ class DataIngesterResource(CRITsAPIResource):
                                 '$arrayElemAt': [
                                     {
                                         '$filter': {
-                                            'input': '$'+EVENT_FIELD+'.objects',
+                                            'input': '$' + EVENT_FIELD + '.objects',
                                             'as': 'obj',
                                             'cond': {'$eq': ['$$obj.type', sub_object_type]}
                                         }
@@ -197,8 +155,7 @@ class DataIngesterResource(CRITsAPIResource):
                     }
                 }
         sort_stage = {'$sort': {EVENT_TIME_RECORDED_FIELD: -1}}
-        limit = self.request.GET.get('limit', '20')
-        limit_stage = {'$limit': int(limit)}
+        limit_stage = {'$limit': 20}
         self.aggregation_pipeline = [
             match_user_submissions_stage,
             project_cleanup_ip_fields_stage,
@@ -212,9 +169,7 @@ class DataIngesterResource(CRITsAPIResource):
         ]
 
     def _add_aggregation_stages_start_at_events(self):
-        # Get all data submitted by user, but query is based on Events collection instead of IPs collection.
-        username = self.request.GET.get('username', '')
-        user_organization = get_user_organization(username)
+        user_organization = 'Comcast'
         match_user_submissions_stage = {
             '$match': {
                 'source.name': user_organization
@@ -239,7 +194,15 @@ class DataIngesterResource(CRITsAPIResource):
             '$project': {
                 IPOutputFields.IP_ADDRESS: '$'+IP_FIELD+'.ip',
                 'created': 1,
+                # {
+                #     '$dateToString': {
+                #         'format': '%Y-%m-%dT%H:%M:%S.%LZ',
+                #         'date': '$created'
+                #     }
+                # },
                 EventOutputFields.ATTACK_TYPES: {
+                    # Hopefully answers "How do I extract 'value' from each object whose type is 'Attack Type'?"
+                    # TODO: now make sure this works
                     '$map': {
                         'input': {
                             '$filter': {
@@ -276,17 +239,137 @@ class DataIngesterResource(CRITsAPIResource):
                         'in': '$$one_obj.value'
                     }
                 }
-        limit = self.request.GET.get('limit', '20')
-        limit_stage = {'$limit': int(limit)}
+        limit_stage = {'$limit': 20}
         self.aggregation_pipeline = [
             match_user_submissions_stage,
-            sort_stage,
             unwind_relationships_stage,
             lookup_ips_stage,
             unwind_ip_field_stage,
             project_event_object_fields_stage,
+            sort_stage,
             limit_stage
         ]
+
+    def _match_ips_on_releasability(self):
+        # Find all ips whose source includes user's organization in source, but not in relasability
+        user_organization = 'Comcast'
+        match_stage = {
+            '$match': {
+                'source.name': user_organization,
+                'releasability.name': {'$ne': user_organization}
+            }
+        }
+        self.aggregation_pipeline.append(match_stage)
+
+    def _project_ip_sub_object_fields(self):
+        """
+        Adds an aggregation stage that projects the values of each IP's sub-objects to top-level fields.
+        :return: (nothing)
+        """
+        project_stage = {
+            '$project': {
+                '_id': 0,
+                IPOutputFields.IP_ADDRESS: '$ip',
+                'relationships': 1,
+            }
+        }
+        self.aggregation_pipeline.append(project_stage)
+
+    def _lookup_related_events(self):
+        """
+        Adds stages that copy events from Events collection that are related to the given IPs.
+        :return: (nothing)
+        """
+        unwind_relationships_stage = {'$unwind': '$relationships'}
+        lookup_stage = {
+            '$lookup': {
+                'from': 'events',
+                'localField': 'relationships.value',
+                'foreignField': '_id',
+                'as': 'event'
+            }
+        }
+        unwind_event_stage = {'$unwind': '$event'}
+        # NOTE: Even though we unwind 'event', we expect that there will only be one new document per each existing
+        # document because of how events are tied to IPs, and we already unwinded the relationships field.
+        stages = [unwind_relationships_stage, lookup_stage, unwind_event_stage]
+        self.aggregation_pipeline.extend(stages)
+
+    def _project_event_fields_to_top_level(self):
+        """
+        Adds stages that temporarily move fields nested in sub-objects of events to top-level fields of the documents.
+        :return: (nothing)
+        """
+        unwind_stage = {'$unwind': '$event.objects'}
+        project_stage = {
+            '$project': {
+                IPOutputFields.IP_ADDRESS: 1,
+                'eventID': '$event._id',
+                'eventtimeRecorded': {
+                    '$dateToString': {
+                        'format': '%Y-%m-%dT%H:%M:%S.%LZ',
+                        'date': '$event.created'
+                    }
+                }
+            }
+        }
+        for event_output_field in EventOutputFields.SUB_OBJECT_FIELDS:
+            sub_object_type = EventOutputFields.get_object_type_from_field_name(event_output_field)
+            project_stage['$project']['event' + event_output_field] = {
+                '$cond': {
+                    'if': {'$eq': ['$event.objects.type', sub_object_type]},
+                    'then': '$event.objects.value',
+                    'else': None
+                }
+            }
+        stages = [unwind_stage, project_stage]
+        self.aggregation_pipeline.extend(stages)
+
+    def _group_documents_by_event_id(self):
+        """
+        Adds a stage that groups repeated documents together by the Event ID.
+        :return: (nothing)
+        """
+        #sort_stage = {'$sort': {'eventID': 1}}
+        group_stage = {
+            '$group': {
+                '_id': '$eventID',
+                IPOutputFields.IP_ADDRESS: {'$first': '$'+IPOutputFields.IP_ADDRESS},
+                'eventtimeRecorded': {'$first': '$eventtimeRecorded'},
+                'eventattackTypes': {'$addToSet': '$eventattackTypes'}
+            }
+        }
+        for event_output_field in EventOutputFields.SUB_OBJECT_FIELDS:
+            if event_output_field != EventOutputFields.ATTACK_TYPES:
+                group_stage['$group']['event' + event_output_field] = {'$max': '$event' + event_output_field}
+        #self.aggregation_pipeline.append(sort_stage)
+        self.aggregation_pipeline.append(group_stage)
+
+    def _project_event_fields_to_nested_level(self):
+        """
+        Adds a stage that moves event fields into a single field named 'event'.
+        :return: (nothing)
+        """
+        project_stage = {
+            '$project': {
+                '_id': 0,
+                IPOutputFields.IP_ADDRESS: 1,
+                'event': {
+                    EventOutputFields.TIME_RECORDED: '$eventtimeRecorded',
+                    EventOutputFields.ATTACK_TYPES: {
+                        '$filter': {
+                            'input': '$eventattackTypes',
+                            'as': 'attackType',
+                            'cond': {'$ne': ['$$attackType', None]}
+                        }
+                    }
+                },
+            }
+        }
+        for event_output_field in EventOutputFields.SUB_OBJECT_FIELDS:
+            if event_output_field != EventOutputFields.ATTACK_TYPES:
+                project_stage['$project']['event'][event_output_field] = '$event' + event_output_field
+        self.aggregation_pipeline.append(project_stage)
 
     def _group_documents_by_ip_with_event_data(self):
         """
@@ -294,7 +377,7 @@ class DataIngesterResource(CRITsAPIResource):
         time we grouped by IP, this stage adds new fields based on the events associated with the IP.
         :return: (nothing)
         """
-        sort_stage = {'$sort': {IPOutputFields.IP_ADDRESS: 1}}
+        #sort_stage = {'$sort': {IPOutputFields.IP_ADDRESS: 1}}
         group_stage = {
             '$group': {
                 '_id': '$' + IPOutputFields.IP_ADDRESS,
@@ -302,7 +385,7 @@ class DataIngesterResource(CRITsAPIResource):
                 IPOutputFields.EVENTS: {'$push': '$event'}
             }
         }
-        self.aggregation_pipeline.append(sort_stage)
+        #self.aggregation_pipeline.append(sort_stage)
         self.aggregation_pipeline.append(group_stage)
 
     def _project_ip_address(self):
@@ -345,34 +428,6 @@ class DataIngesterResource(CRITsAPIResource):
         sort_stage = {'$sort': {IPOutputFields.LAST_TIME_RECEIVED: -1}}
         self.aggregation_pipeline.append(sort_stage)
 
-    def dehydrate(self, bundle):
-        """
-        Move bundle data to correct attribute, and convert number fields from strings to integers or floats.
 
-        :param bundle:
-        :return:
-        """
-        bundle.data = bundle.obj
-        # Convert appropriate fields of events to integers.
-        for integer_field in EventOutputFields.INTEGER_FIELDS:
-            if bundle.data.get(integer_field):
-                try:
-                    bundle.data[integer_field] = int(bundle.data[integer_field])
-                except (TypeError, ValueError):
-                    pass
-        return bundle
-
-    def alter_list_data_to_serialize(self, request, data):
-        """
-        Note: This function is called within get_list() of resources.py (tastypie library) after calling
-        dehydrate() (defined in this class) on each bundle object.
-
-        :param request:
-        :param data:
-        :return:
-        """
-        del data['meta']
-        username = request.GET.get('username', '')
-        source_name = get_user_organization(username)
-        data['ProviderName'] = source_name
-        return data
+test = IngestAggregationSpeedTest()
+test.run()
