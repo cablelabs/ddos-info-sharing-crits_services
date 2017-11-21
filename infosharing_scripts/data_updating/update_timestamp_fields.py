c8903f54beaaa98b62d54329a4a21ccfeb06c54a
Zane Hintzman <z.hintzman@cablelabs.com>
2017-11-20 17:01:35 -0700

    Improved timestamp-fixing script's speed by using pymongo instead of CRITs.

--- infosharing_scripts/data_updating/update_timestamp_fields.py
+++ infosharing_scripts/data_updating/update_timestamp_fields.py
@@ -7,47 +7,14 @@ local timezone (MST). The values will be converted to UTC. These are the fields 
 IP: created, modified, Last Time Received
 Event: created, modified
 
-When new values are saved to an object, the 'modified' time of that object should automatically be set to a legitimate
-UTC time (specifically the time when the values were saved).
+When new values are saved to an object, the 'modified' time of that object will be set to a legitimate UTC time
+(specifically the time when the values were saved).
 """
-
-import os
 from datetime import datetime
-from multiprocessing import Pool
 import pytz
 from tzlocal import get_localzone
-import pandas as pd
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'crits.settings'
-
-# This is weird, but for some reason if I don't call get_localzone() before importing from CRITs, it will return "UTC"
-# instead of the actual timezone of the machine (which in our case should be "America/Denver"). Without this line,
-# the local_time_to_utc() function would just return the same value as the input.
-print get_localzone()
-
-from crits.core.user_tools import get_user_organization
-from crits.events.event import Event
-from crits.ips.ip import IP
-
-
-# Function borrowed from the internet
-def iterator2dataframes(iterator, chunk_size):
-    """
-    Turn an iterator into multiple small pandas.DataFrame objects. This is a balance between memory and efficiency.
-    :param iterator:
-    :param chunk_size:
-    :return: pandas.DataFrame
-    """
-    records = []
-    frames = []
-    for i, record in enumerate(iterator):
-        records.append(record)
-        if i % chunk_size == chunk_size - 1:
-            frames.append(pd.DataFrame(records))
-            records = []
-    if records:
-        frames.append(pd.DataFrame(records))
-    return pd.concat(frames)
+from pymongo import MongoClient
+import pendulum
 
 
 def local_time_to_utc(local_datetime):
@@ -62,65 +29,59 @@ def local_time_to_utc(local_datetime):
     return utc_time
 
 
-def update_ip_object(ip_object):
-    analyst = 'analysis_autofill'
-    ip_object.created = local_time_to_utc(ip_object.created)
-    # To prevent skipping objects while iterating through sub-objects, store list of objects to remove later.
-    previous_object_values = []
-    for o in ip_object.obj:
-        if o.object_type == 'Last Time Received':
-            previous_object_values.append(o.value)
-    last_time_seen = ''
-    for previous_value in previous_object_values:
-        last_time_seen = previous_value
-        ip_object.remove_object('Last Time Received', previous_value)
-    try:
-        last_time_seen_datetime = datetime.strptime(last_time_seen, "%Y-%m-%dT%H:%M:%S.%fZ")
-        last_time_seen_datetime = local_time_to_utc(last_time_seen_datetime)
-        last_time_seen = last_time_seen_datetime.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-        ip_object.add_object('Last Time Received', last_time_seen, get_user_organization(analyst), '', '', analyst)
-    except ValueError:
-        pass
-    ip_object.status = "Analyzed"
-    ip_object.save(username=analyst)
-    return
+in_progress_filter = {'status': 'In Progress'}
+client = MongoClient()
+ips = client.crits.ips
+ip_objects = ips.find(filter=in_progress_filter)
+number_of_ips = 0
 
-
-def update_event_object(event_object):
-    analyst = 'analysis_autofill'
-    event_object.created = local_time_to_utc(event_object.created)
-    event_object.status = "Analyzed"
-    event_object.save(username=analyst)
-    return
-
-
-ip_objects = IP.objects(status='In Progress')
 start_time = datetime.now()
-ip_objects_list = list(ip_objects)
-duration = datetime.now() - start_time
-print "Time to convert IPs to list:", duration
-
-event_objects = Event.objects(status='In Progress')
-start_time = datetime.now()
-event_objects_list = list(event_objects)
-duration = datetime.now() - start_time
-print "Time to convert Events to list:", duration
-
-pool = Pool(10)
-print "Pool initialized."
-start_time = datetime.now()
-#for ip_object in ip_objects:
-#    update_ip_object(ip_object)
-pool.map(update_ip_object, ip_objects_list)
+for ip_object in ip_objects:
+    new_created_date = local_time_to_utc(ip_object['created'])
+    new_last_time_seen_object = None
+    for obj in ip_object['objects']:
+        if obj['type'] == 'Last Time Received':
+            last_time_seen = obj['value']
+            last_time_seen_datetime = datetime.strptime(last_time_seen, "%Y-%m-%dT%H:%M:%S.%fZ")
+            last_time_seen_datetime = local_time_to_utc(last_time_seen_datetime)
+            new_last_time_seen = last_time_seen_datetime.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            new_last_time_seen_object = obj
+            new_last_time_seen_object['value'] = new_last_time_seen
+    query = {'_id': ip_object['_id']}
+    update_operators_part1 = {'$pull': {'objects': {'type': 'Last Time Received'}}}
+    update_operators_part2 = {
+        '$set': {
+            'created': new_created_date,
+            'modified': pendulum.now('UTC'),
+            'status': 'Analyzed',
+        },
+        '$push': {'objects': new_last_time_seen_object}
+    }
+    ips.update_one(filter=query, update=update_operators_part1)
+    ips.update_one(filter=query, update=update_operators_part2)
+    number_of_ips += 1
 duration = datetime.now() - start_time
 print "Time to update IPs:", duration
-print "Number of IPs:", len(ip_objects_list)
+print "Number of IPs:", number_of_ips
+
+
+events = client.crits.events
+event_objects = events.find(filter=in_progress_filter)
+number_of_events = 0
 
 start_time = datetime.now()
-#for event_object in event_objects:
-#    update_event_object(event_object)
-pool.map(update_event_object, event_objects_list)
+for event_object in event_objects:
+    new_created_date = local_time_to_utc(event_object['created'])
+    query = {'_id': event_object['_id']}
+    update_operators = {
+        '$set': {
+            'created': new_created_date,
+            'modified': pendulum.now('UTC'),
+            'status': 'Analyzed'
+        }
+    }
+    events.update_one(filter=query, update=update_operators)
+    number_of_events += 1
 duration = datetime.now() - start_time
 print "Time to update Events:", duration
-print "Number of Events:", len(event_objects_list)
-pool.close()
+print "Number of Events:", number_of_events
